3f7548a1b20fe443240d1a79a20717135bd704de
Emon02 <abdulalimemon11@gmail.com>
2022-11-03 02:07:51 +0600

    SetUp

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        }
+      }
+    ],
+  },
+  plugins: [require("daisyui")],
+}
