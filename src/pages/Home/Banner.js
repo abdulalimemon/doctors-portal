0bdb493115d2114a7b84110490b0388260563e26
Emon02 <abdulalimemon11@gmail.com>
2022-11-03 03:01:49 +0600

    banner section

---
+++ src/pages/Home/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import bannerBg from '../../assets/images/chair.png';
+const Banner = () => {
+    return (
+        <div className="hero min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={bannerBg} className="mt-3 lg:mt-0 lg:w-1/2 rounded-lg shadow-2xl" alt='Banner'/>
+                <div>
+                    <h2 className="text-3xl mt-5 lg:mt-0 lg:text-5xl font-bold text-accent">Your New Smile Starts Here</h2>
+                    <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
+                    <button className="btn btn-secondary text-white">GET STARTED</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/pages/Shared/Navbar.js
+++ src/pages/Shared/Navbar.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const menuItems = <>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/appointment'>Appointment</Link></li>
+        <li><Link to='/review'>Reviews</Link></li>
+        <li><Link to='/contact'>Contact Us</Link></li>
+        <li><Link to='/login'>Login</Link></li>
+    </>
     return (
-        <div className="container navbar bg-base-100">
+        <div className="container mx-auto navbar bg-base-100">
             <div className="navbar-start">
                 <Link className="btn btn-ghost normal-case text-xl" to='/'>Doctors Portal</Link>
             </div>
             <div className="navbar-end hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
-                    <li><Link>Home</Link></li>
-                    <li><Link>About</Link></li>
-                    <li><Link>Appointment</Link></li>
-                    <li><Link>Reviews</Link></li>
-                    <li><Link>Contact Us</Link></li>
-                    <li><Link>Login</Link></li>
+                    {menuItems}
                 </ul>
             </div>
             <div className="navbar-end  lg:hidden">
@@ -23,12 +26,7 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link>Home</Link></li>
-                        <li><Link>About</Link></li>
-                        <li><Link>Appointment</Link></li>
-                        <li><Link>Reviews</Link></li>
-                        <li><Link>Contact Us</Link></li>
-                        <li><Link>Login</Link></li>
+                        {menuItems}
                     </ul>
                 </div>
             </div>
