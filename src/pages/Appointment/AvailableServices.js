a379a9c7a63b8d6cdb3391db948163ac51847608
Emon02 <abdulalimemon11@gmail.com>
2022-11-06 23:10:36 +0600

    firebase setup

--- src/pages/Appointment/AvailableServices.js
+++ src/pages/Appointment/AvailableServices.js
@@ -10,7 +10,7 @@ const AvailableServices = ({ date }) => {
     const [treatment, setTreatment] = useState(null);
 
     useEffect(() => {
-        fetch('services.json')
+        fetch('http://localhost:5000/services')
             .then(res => res.json())
             .then(data => setServices(data));
     }, [services]);
