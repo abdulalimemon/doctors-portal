05c3fc8fe741aabdde5f1a928724b8dcbd841e5f
Emon02 <abdulalimemon11@gmail.com>
2022-11-13 23:30:23 +0600

    booking data collection on mongodb database

--- src/pages/Appointment/BookingModal.js
+++ src/pages/Appointment/BookingModal.js
@@ -7,13 +7,36 @@ import auth from '../../firebase.init';
 const BookingModal = ({ date, treatment, setTreatment }) => {
     const { _id, name, slots } = treatment;
     const [user] = useAuthState(auth);
+    const formattedDate = format(date, 'PP');
 
     const handleBooking = event => {
         event.preventDefault();
         const slot = event.target.slot.value;
-        console.log(_id, name, slot);
-        setTreatment(null);
-    }
+
+        const booking = {
+            treatmentId: _id,
+            treatmentName: name,
+            date: formattedDate,
+            slot,
+            patientEmail: user.email,
+            patientName: user.displayName,
+            phoneNumber: event.target.phone.value
+        }
+
+        fetch('http://localhost:5000/booking', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                // to close the modal
+                setTreatment(null);
+            })
+    };
+
     return (
         <>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
